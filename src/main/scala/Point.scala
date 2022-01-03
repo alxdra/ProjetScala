b9bef0fd7a13aa9b0f51044f5d6071093f233ab5
alxdra <37247366+alxdra@users.noreply.github.com>
2022-01-03 14:18:38 +0100

    Creation ADT

--- src/main/scala/Point.scala
+++ src/main/scala/Point.scala
@@ -1,12 +1,17 @@
-case class Point(x: Float, y:Float, z:Float = 0)
+sealed abstract class Point{
+}
+
+
+case class Point2D(x: Float, y:Float) extends Point
+case class Point3D(x:Float, y :Float, z:Float) extends Point
 
 object Point{
     def apply(st : String): Option[Point] = {
         st.split(",")
             .map(_.trim) //trim retire les espaces 
              match{
-                case Array(x,y,z) => Some(Point(x.toFloat, y.toFloat, z.toFloat))
-                case Array(x,y) => Some(Point(x.toFloat, y.toFloat))
+                case Array(x,y,z) => Some(Point3D(x.toFloat, y.toFloat, z.toFloat))
+                case Array(x,y) => Some(Point2D(x.toFloat, y.toFloat))
                 case _ => None
             }
     }
