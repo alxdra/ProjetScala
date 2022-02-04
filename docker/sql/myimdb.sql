67dcf06dca5ada6ee146b08fa4e7d7bf520a5ac8
KugiSath <96889937+KugiSath@users.noreply.github.com>
2022-02-04 21:08:07 +0100

    Add files via upload

---
+++ docker/sql/myimdb.sql
@@ -0,0 +1,20 @@
+-- Database
+CREATE DATABASE myimdb;
+\c myimdb;
+
+-- Country
+CREATE TABLE country (
+
+);
+
+-- Airport
+CREATE TABLE airport (
+
+);
+
+-- runways
+CREATE TABLE actors (
+ 
+);
+
+-- Lien entre les tables
