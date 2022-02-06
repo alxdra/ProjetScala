5de61e0c0b0bc6252600929f4b75c250904d0f9e
alxdra <37247366+alxdra@users.noreply.github.com>
2022-02-06 15:15:15 +0100

    Début Partie 2

--- src/main/scala/Country.scala
+++ src/main/scala/Country.scala
@@ -3,25 +3,36 @@ case class Country(id : Int, //Id_country,
              code : String,
              name : String,
              continent : String,
-             wikipedia_link : String,
-             keywords : List[String] //list
-            )
+             keywords : List[String], //list
+             airports: Option[List[Airport]]
+            ){
+
+        def addAirports(a : List[Airport]): List[Airport]={
+            a.foldLeft(List[Airport]())((list, currentAirport)=>{
+                currentAirport.iso_country 
+                        match {
+                        case `code` => currentAirport :: list
+                        case _ => list
+                }
+            })
+        }
+}
 
 //class Id_country(val x:Int) extends AnyVal
 //Id en value class ?
 object Country  {
     def fromCSV[A](line : Array[String]): Either[String, Country] = {
-        (Try(line(0).toInt).toOption, line.lift(1), line.lift(2), line.lift(3), line.lift(4),line.lift(5), line.lift(6))
+        (Try(line(0).toInt).toOption, line.lift(1), line.lift(2), line.lift(3), line.lift(5), line.lift(6))
              match{
-                case (Some(id), Some(code), Some(name), Some(continent), Some(wiki_link), Some(k1), Some(k2)) =>
+                case (Some(id), Some(code), Some(name), Some(continent), Some(k1), Some(k2)) =>
                         //Right(Country(new Id_country(id),code, name, continent, wiki_link, List(k1,k2)))
-                        Right(Country(id,code, name, continent, wiki_link, List(k1,k2)))
+                        Right(Country(id,code, name, continent, List(k1,k2), None))
 
-                case (Some(id), Some(code), Some(name), Some(continent), Some(wiki_link), Some(k1), None) =>
-                        Right(Country(id,code, name, continent, wiki_link, List(k1)))
+                case (Some(id), Some(code), Some(name), Some(continent), Some(k1), None) =>
+                        Right(Country(id,code, name, continent, List(k1), None))
 
-                case (Some(id), Some(code), Some(name), Some(continent), Some(wiki_link), None, None) =>
-                        Right(Country(id,code, name, continent, wiki_link, Nil))
+                case (Some(id), Some(code), Some(name), Some(continent), None, None) =>
+                        Right(Country(id,code, name, continent, Nil, None))
 
                 case _ => Left("Error to build country")
             }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,38 @@
 import scala.io.Source
+import javax.management.monitor.CounterMonitor
 
 object Main extends App {
 
-  val listofCountries = readCSV("data/countries.csv",Country.fromCSV)
-  listofCountries.foreach(println)
+  val a = Airport(12,"CDG","big","Don't Crash",None,None,None,None,"S",None, None, None, None, None, None, None, None,None)
+  val b = Airport(12,"CDG","big","Don't Crash",None,None,None,None,"O",None, None, None, None, None, None, None, None,None)
+  val l = List(a,b)
+  
+  val c = Country(1,
+                  "S",
+                  "Lalaland",
+                  "NeverLand",
+                   Nil,
+                   None)
 
-// val listofAirports = readCSV("data/airports.csv", Airport.fromCSV)
-  //listofAirports.foreach(println)
+  
+  c.airports = Some(c.addAirports(l))
+  println(c.airports)
+
+ /* val allCountries = readCSV("data/countries.csv",Country.fromCSV).colle
+ ct{
+        case Right(x)=>x 
+        case Left(x)=>None
+      }
+  
+  allCountries.foreach(println)
+
+  val allAirports = readCSV("data/airports.csv", Airport.fromCSV).collect{
+        case Right(x)=>x 
+        case Left(x)=>None
+      }
+  //listofAirports.foreach(println)*/
+
+ // allCountries(1).addAirports(allAirports, Nil)
 
  //val listofRunways = readCSV("data/runways.csv", Runway.fromCSV)
  //listofRunways.foreach(println)
@@ -14,7 +40,7 @@ object Main extends App {
 
   def readCSV[T](fileName :String, 
                 fonctionObject : Array[String]=> Either[String,T], 
-                )  : List[ Either[String,T]] = {
+                )  : List[Either[String,T]]= {
 
     val bufferedSource = Source.fromFile(fileName)
     val fileList = bufferedSource.getLines
