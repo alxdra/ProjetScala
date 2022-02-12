60a87205202975253474227fd7eb2b87f37e1227
KugiSath <96889937+KugiSath@users.noreply.github.com>
2022-02-12 22:29:42 +0100

    improve

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -19,39 +19,72 @@ object Main extends App {
   commandlineuser(allAirports,allRunways,allCountries)
 
   def commandlineuser(Airport : List[Airport] , Runways: List[Runway] , Country : List[Country]) : Any = {
-    println("------------------------------------------")
-    println("Que voulez-vous faire ?(entrez un numero")
-    println("1. query")
-    println("2. reports")
-    println("3. exit")
+    println("\n \n ------------------MAIN MENU------------------------ \n")
+    println("Que voulez-vous faire ?(entrez un numero)")
+    println("1. Query")
+    println("2. Reports")
+    println("3. Exit")
+    println("\n ==> \n ")
     scala.io.StdIn.readLine() match {
-      case "1" => commandlinequery(allAirports,allRunways,allCountries)
-      case "2" => commandlinereports(allAirports,allRunways,allCountries)
-      case "3" => println("exit")
-      case _ => println("error")
+      case "1" | "Query" | "query" => commandlinequery(allAirports,allRunways,allCountries)
+      case "2" | "Reports" | "reports" => commandlinereports(allAirports,allRunways,allCountries)
+      case "3" | "Exit" | "exit" => println("... Exit ... \n \n")
+      case _ => println("ERROR. Enter a correct option.\n")
+        commandlineuser(allAirports,allRunways,allCountries)
     }
   }
 
-  def commandlinequery(Airport : List[Airport] , Runways: List[Runway] , Country : List[Country]) : Any = { 
-    println("Entrez un nom de pays ou code. Ex : 'Andorra' ou 'AD', 'France' ou 'FR' ")
-    displayAirports(scala.io.StdIn.readLine())
+  def commandlinequery(Airport : List[Airport] , Runways: List[Runway] , Country : List[Country]) : Any = {
+    println("\n \n ------------------QUERY MENU------------------------ \n")
+    println("--Query--")
+    println("1. Display the airports & runways at each airport for a country")
+    println("2. Back to menu")
+    println("3. Exit")
+    println("\n ==> \n ")
+     scala.io.StdIn.readLine() match {
+      case "1" | "Display"  => println("Entrez un nom de pays ou code. Ex : 'Andorra' ou 'AD', 'France' ou 'FR' ")
+        println("==>")
+        displayAirports(scala.io.StdIn.readLine())
+        commandlinequery(allAirports,allRunways,allCountries)
+      case "2" | "Back" | "Back to menu" => println("... Going back to menu ...")
+        commandlineuser(allAirports,allRunways,allCountries)
+      case "3" | "Exit" | "exit" => println("... Exit ... \n \n")
+      case _ => println("ERROR. Enter a correct option. \n")
+        commandlinequery(allAirports,allRunways,allCountries)
+    }
     
-    commandlineuser(allAirports,allRunways,allCountries)
 
   }
 
   def commandlinereports(Airport : List[Airport] , Runways: List[Runway] , Country : List[Country]) : Any = { 
-    println("1. 10 countries with highest number of airports (with count) and countries  with lowest number of airports.")
-    println("2. Type of runways per country")
-    println("3. The top 10 most common runway latitude")
+    println("\n \n ------------------REPORTS MENU------------------------ \n")
+    println("--Reports--")
+    println("Quelle requête voulez-vous faire ? ")
+    println("1. Top10 country : 10 countries with highest number of airports (with count) and countries  with lowest number of airports.")
+    println("2. Runways type : Type of runways per country")
+    println("3. Top10 runways : The top 10 most common runway latitude")
+    println("4. Back to menu")
+    println("5. Exit")
+    println("\n ==> \n ")
 
     scala.io.StdIn.readLine() match {
-      case "1" => topCountries()
-      case "2" => typeRunways()
-      case "3" => commonLatitud()
-      case _ => println("error")
+      case "1" | "Top10 country" | "top10 country"=> println("\n ---RESULTS-- \n")
+        topCountries()
+        commandlinereports(allAirports,allRunways,allCountries)
+      case "2" | "Runways type" | "runways type" =>  println("\n ---RESULTS-- \n")
+        typeRunways()
+        commandlinereports(allAirports,allRunways,allCountries)
+      case "3" | "Top10 runways" | "top10 runways" => println("\n ---RESULTS-- \n")
+        commonLatitud()
+        commandlinereports(allAirports,allRunways,allCountries)
+      case "4"| "Back" | "Back to menu" => println("... Going back to menu ... \n ")
+       commandlineuser(allAirports,allRunways,allCountries)
+      case "5" | "Exit" | "exit" => println("... Exit ... \n \n")
+      case _ => println("Error, enter a correct option. \n")
+        commandlinereports(allAirports,allRunways,allCountries)
     }
-      commandlineuser(allAirports,allRunways,allCountries)
+
+
 
   }
   
